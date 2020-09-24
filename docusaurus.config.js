48765b762207271eb51f5d47deff6ffdf5635c04
hugo.p.costa <hugo.p.costa@truphone.com>
2020-09-24 17:38:22 +0100

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   onBrokenLinks: 'throw',
   favicon: 'img/visualorc.ico',
   organizationName: 'visorc', // Usually your GitHub org/user name.
-  projectName: 'visorc.github.io', // Usually your repo name.
+  projectName: 'visualorc.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'VisualOrc',
